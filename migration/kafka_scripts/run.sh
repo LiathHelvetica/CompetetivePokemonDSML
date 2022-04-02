83b5a4c28511aa88bd03bfd57919d072f5a7185e
liath <matthewdragun@gmail.com>
2022-04-02 23:30:45 +0200

    [main] updated run kafka script

--- migration/kafka_scripts/run.sh
+++ migration/kafka_scripts/run.sh
@@ -5,9 +5,11 @@ KAFKA_HOME="/home/liath/Kafka"
 cd ${KAFKA_HOME} || exit
 
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
-sleep 2
+sleep 5
 
-nohup bin/migration-server-start.sh config/server.properties &
-sleep 2
+nohup bin/kafka-server-start.sh config/server.properties &
+sleep 5
 
-docker run -d --network=host -p 8080:8080 -e KAFKA_BROKERS=localhost:20300 quay.io/cloudhut/kowl:master
+docker run -d --network=host -p 20301:20301 -e KAFKA_BROKERS=localhost:20300 quay.io/cloudhut/kowl:master
+
+bin/kafka-topics.sh --create --topic pokemon-export --bootstrap-server localhost:20300
